f79b5096e6abb196785a15dcdb5623d721a33781
Matias Aguirre <maguirre@matware.com.ar>
2014-10-03 17:56:55 -0300

    Update websocket-example cli script to use Matware libraries

--- bootstrap.php
+++ bootstrap.php
@@ -3,38 +3,60 @@
  * @version       $Id: 
  * @package       Matware
  * @subpackage    ExampleWebSocket
- * @copyright     Copyright (C) 1996 - 2011 Matware - All rights reserved.
+ * @copyright     Copyright (C) 1996 - 2014 Matware - All rights reserved.
  * @author        Matias Aguirre
  * @email         maguirre@matware.com.ar
  * @link          http://www.matware.com.ar/
  * @license       GNU/GPL http://www.gnu.org/licenses/gpl-2.0-standalone.html
  */
 
-/*
- * Change the following path to suit where you have cloned or installed the Joomla Platform repository.
- * The default setting assumes you have the joomla platform and examples folder at the same level.
- */
-
 // Setup the base path related constant.
-define('JPATH_BASE', dirname(__FILE__));
-define('JPATH_SITE', dirname(__FILE__));
-define('JPATH_ROOT', dirname(__FILE__));
-define('JPATH_LIBRARIES', dirname(dirname(__FILE__)).'/joomla-platform/libraries' );
+$ROOT = dirname(dirname(__FILE__)).'/joomla-cms.my';
 
-// Import the Joomla! Platform
-require JPATH_LIBRARIES.'/import.php';
+// Ensure that required path constants are defined.
+if (!defined('JPATH_LIBRARIES'))
+{
+	define('JPATH_LIBRARIES', $ROOT.'/libraries');
+}
+// Fire up the Platform importer.
+if (file_exists(JPATH_LIBRARIES . '/import.php'))
+{
+	require JPATH_LIBRARIES . '/import.php';
+}
+// Import the Joomla! CMS
+if (file_exists(JPATH_LIBRARIES.'/cms.php')) {
+	require_once JPATH_LIBRARIES.'/cms.php';
+}
+// Ensure that required path constants are defined.
+if (!defined('JPATH_SITE'))
+{
+	define('JPATH_SITE', $ROOT);
+}
+// Define cache path
+if (!defined('JPATH_CACHE'))
+{
+	define('JPATH_CACHE', $ROOT.'/cache');
+}
 
-// Import the JCli class from the platform.
-jimport('joomla.application.cli');
-// Import the JDatabase class
+// Import the database libraries
 jimport('joomla.database.database');
-// Import the JLog class
-jimport('joomla.error.log');
+// Import the file libraries
+jimport('joomla.filesystem.file');
+// Import the html libraries
+jimport('cms.html.html');
 
-// Import the JWebSocket class from the platform.
-require JPATH_LIBRARIES.'/joomla/socket/websocket.php';
+// Import the MWebSocket class from Matware libraries.
+JLoader::register('MSocketDaemon', JPATH_LIBRARIES.'/matware/socket/daemon.php');
+JLoader::register('MWebsocket', JPATH_LIBRARIES.'/matware/socket/websocket.php');
+JLoader::register('MWebsocketUser', JPATH_LIBRARIES.'/matware/socket/websocket/user.php');
 
-// Get config value
-$config = JFactory::getConfig('./configuration.php');
-// Define debug
-define('JDEBUG', $config->get('debug'));
+// Interfaces.
+JLoader::register('MWebsocketController', JPATH_LIBRARIES.'/matware/socket/websocket/controller.php');
+JLoader::register('MWebsocketHandshake', JPATH_LIBRARIES.'/matware/socket/websocket/handshake.php');
+JLoader::register('MWebsocketProtocol', JPATH_LIBRARIES.'/matware/socket/websocket/protocol.php');
+
+// Adapters.
+JLoader::discover('MWebsocketHandshake', JPATH_LIBRARIES.'/matware/socket/websocket/handshakes');
+JLoader::discover('MWebsocketProtocol', JPATH_LIBRARIES.'/matware/socket/websocket/protocols');
+
+require __DIR__ . '/configuration.php';
